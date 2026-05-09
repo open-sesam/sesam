8eec3eb98d48a965d5658cc0732f01b1fe3ab89b
Johnny <fishermans-enemy@hotmail.de>
2026-05-09 17:39:29 +0200

    rough first implementation of ast parsing and include resolve

--- cli/flags.go
+++ cli/flags.go
@@ -1,8 +1,10 @@
 package cli
 
 import (
+	"context"
 	"time"
 
+	"github.com/open-sesam/sesam/config"
 	"github.com/urfave/cli/v3"
 )
 
@@ -50,6 +52,33 @@ var flagsInit = []cli.Flag{
 	&cli.BoolFlag{
 		Name:  "use-root",
 		Usage: "Initialize in the selected directory even when it already contains many files",
+	},
+}
+
+var flagsModifyAddSecret = []cli.Flag{
+	&cli.StringFlag{
+		Name:  "type",
+		Usage: "Type of secret (ssh_key,password,template,custom)",
+		Action: func(_ context.Context, _ *cli.Command, v string) error {
+			return config.VerifySecretType(v)
+		},
+	},
+	&cli.StringFlag{
+		Name:     "path",
+		Required: true,
+		Usage:    "Path to the secret which should be added",
+	},
+	&cli.StringFlag{
+		Name:  "name",
+		Usage: "Name of the secret (filename if empty)",
+	},
+	&cli.StringSliceFlag{
+		Name:  "access",
+		Usage: "Group with access to this secret (can be given multiple times)",
+	},
+	&cli.StringFlag{
+		Name:  "description",
+		Usage: "Description of the secret",
 	},
 }
 
