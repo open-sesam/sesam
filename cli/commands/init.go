0efbe5fb406905732e25a46cab7b0e573c0d09d6
Chris Pahl <sahib@online.de>
2026-05-31 18:11:25 +0200

    feat: very important life altering feature

--- cli/commands/init.go
+++ cli/commands/init.go
@@ -11,6 +11,16 @@ import (
 
 // TODO: Show ascii logo on init
 
+const (
+	asciiLogo = `
+,adPPYba,   ,adPPYba,  ,adPPYba,  ,adPPYYba,  88,dPYba,,adPYba,
+I8[    ""  a8P_____88  I8[    ""  ""     'Y8  88P'   "88"    "8a
+ '"Y8ba,   8PP"""""""   '"Y8ba,   ,adPPPPP88  88      88      88
+aa    ]8I  "8b,   ,aa  aa    ]8I  88,    ,88  88      88      88
+'"YbbdP"'   '"Ybbd8"'  '"YbbdP"'  '"8bbdP"Y8  88      88      88
+`
+)
+
 // HandleInit bootstraps sesam metadata in a git repository.
 func HandleInit(ctx context.Context, cmd *cli.Command) (err error) {
 	initialUser := cmd.String("user")
@@ -42,6 +52,8 @@ func HandleInit(ctx context.Context, cmd *cli.Command) (err error) {
 		}
 		slog.Warn("close repo failed", slog.Any("error", closeErr))
 	}()
+
+	fmt.Println(asciiLogo)
 
 	return nil
 }
