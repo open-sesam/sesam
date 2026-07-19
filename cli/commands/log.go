a7b47b2934e5e79416e610301d7df235339858b9
Chris Pahl <sahib@online.de>
2026-07-19 12:36:34 +0200

    Feat/doctor (#82)
    * feat: add start of sesam doctor
    
    * feat: implement almost full set of doctor commands
    
    * fix: add identity-type
    
    * fix: lock was missing in gitgnore

--- cli/commands/log.go
+++ cli/commands/log.go
@@ -72,7 +72,7 @@ func describeLogEntry(out *termenv.Output, e *core.AuditEntrySigned, full bool) 
 	secretColor := termenv.ANSIBrightMagenta
 	repoColor := termenv.ANSIBrightGreen
 	groupColor := termenv.ANSIBrightYellow
-	dim := out.Color("#808080")
+	dim := out.Color(colorGrey)
 
 	c := func(v any, col termenv.Color) string {
 		return out.String(fmt.Sprintf("%v", v)).Foreground(col).String()
@@ -215,7 +215,7 @@ func HandleLog(ctx context.Context, cmd *cli.Command, r *repo.Repo) error {
 
 	full := cmd.Bool("full")
 	out := termenv.NewOutput(os.Stdout)
-	dim := out.Color("#808080")
+	dim := out.Color(colorGrey)
 	timeColor := out.Color("#008000")
 	userColor := termenv.ANSIBrightCyan
 
